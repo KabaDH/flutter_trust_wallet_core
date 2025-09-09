49e293a27958a1b3d00e3672fa1a2d9c97ac67c1
inlevkov <io.levk@gmail.com>
2025-09-09 12:11:56 +0200

    added lib for cli

--- lib/flutter_trust_wallet_core.dart
+++ lib/flutter_trust_wallet_core.dart
@@ -3,40 +3,53 @@ library flutter_trust_wallet_core;
 import 'dart:ffi';
 import 'dart:io';
 import 'dart:typed_data';
+
 import 'package:ffi/ffi.dart';
+import 'package:path/path.dart' as path;
 
 import 'trust_wallet_core_ffi.dart';
 
-
-part 'extensions.dart';
-
-part 'core/mnemonic.dart';
+part 'core/any_address.dart';
+part 'core/any_signer.dart';
+part 'core/base58.dart';
+part 'core/bitcoin_address.dart';
+part 'core/bitcoin_script.dart';
+part 'core/coin_type_configuration.dart';
+part 'core/ethereum_abi.dart';
+part 'core/ethereum_abi_function.dart';
+part 'core/ethereum_abi_value.dart';
+part 'core/fio_account.dart';
+part 'core/groestlcoin_address.dart';
+part 'core/hash.dart';
+part 'core/hd_version.dart';
 part 'core/hd_wallet.dart';
+part 'core/hrp.dart';
+part 'core/mnemonic.dart';
 part 'core/private_key.dart';
 part 'core/public_key.dart';
-part 'core/stored_key.dart';
-part 'core/any_address.dart';
-part 'core/base58.dart';
-part 'core/hash.dart';
-part 'core/bitcoin_address.dart';
-part 'core/any_signer.dart';
-part 'core/bitcoin_script.dart';
 part 'core/ripple_x_address.dart';
 part 'core/segwit_address.dart';
 part 'core/solana_address.dart';
-part 'core/hd_version.dart';
-part 'core/groestlcoin_address.dart';
-part 'core/fio_account.dart';
-part 'core/ethereum_abi.dart';
-part 'core/ethereum_abi_value.dart';
-part 'core/coin_type_configuration.dart';
-part 'core/ethereum_abi_function.dart';
-part 'core/hrp.dart';
-
-
+part 'core/stored_key.dart';
+part 'extensions.dart';
 
 class FlutterTrustWalletCore {
-  static void init() {
-    walletCoreLib = Platform.isAndroid ? DynamicLibrary.open("libTrustWalletCore.so") : DynamicLibrary.process();
+  /// `libTrustWalletCore.dylib` will not be imported into the application
+  /// through the plugin. It must be manually placed into the `lib/cli`
+  /// directory of the project that uses the plugin.
+  static void init({bool useCliLib = false}) {
+    if (useCliLib) {
+      final libraryPath = path.join(
+        Directory.current.path,
+        'lib',
+        'cli',
+        'libTrustWalletCore.dylib',
+      );
+      walletCoreLib = DynamicLibrary.open(libraryPath);
+      return;
+    }
+    walletCoreLib = Platform.isAndroid
+        ? DynamicLibrary.open("libTrustWalletCore.so")
+        : DynamicLibrary.process();
   }
 }
